32eaabaf081961cd453b99cd127323d41b7cf290
rv <m0029.swim@gmail.com>
2015-12-31 00:13:17 +0900

    update

--- collect/scripts/realtime_master.sh
+++ collect/scripts/realtime_master.sh
@@ -1,4 +1,4 @@
-node ${TT_HOME}/collect/node/investing/investing_websocket.js &
+node ${TT_HOME}/collect/investing/investing_websocket.js &
 echo "investing_websocket.js" > pid.txt
 echo $! >> pid.txt
 cat pid.txt
